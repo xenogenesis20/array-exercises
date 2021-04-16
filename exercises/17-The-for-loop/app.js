14ca3ea503f16aa5f5fafe875877cdf1511a2ae7
xenogenesis20 <xenogenesis20@outlook.com>
2021-04-16 20:21:24 +0000

    more progress

--- exercises/17-The-for-loop/app.js
+++ exercises/17-The-for-loop/app.js
@@ -1,7 +1,8 @@
 var myArray = [2323,4344,2325,324413,21234,24531,2123,42234,544,456,345,42,5445,23,5656,423];
 
+let sum = 0;
 for (var index in myArray){
-
-	console.log(myArray[index]);
-	
+    sum += myArray[index];
+    var average = sum / myArray.length;
 }
+console.log(average)
